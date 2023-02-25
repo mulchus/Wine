7d1785c6cfccc919737f460c0664bbe237475996
mulchus <mulch@list.ru>
2023-02-25 20:44:19 +0300

    Did file path from a command line or setup.txt

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from datetime import datetime
 import pandas
 import collections
-
+import argparse
+import os
+from environs import Env
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
@@ -22,6 +24,11 @@ def choosing_year_prefix(age):
     return year_prefix
 
 
+def get_wines_from_file(path):
+    wines = pandas.read_excel(path, na_values='-', keep_default_na=False).to_dict('records')
+    return wines
+
+
 def main():
     env = Environment(
         loader=FileSystemLoader('.'),
@@ -31,8 +38,31 @@ def main():
     template = env.get_template('template.html')
 
     age = datetime.today().year - ORGANIZATION_CREATION_YEAR
-    wines = pandas.read_excel('wine3.xlsx', na_values='-', keep_default_na=False).to_dict('records')
 
+    wine_parser = argparse.ArgumentParser(description='Сайт магазина авторского вина "Новое русское вино"')
+    wine_parser.add_argument(
+        'path',
+        nargs='?',
+        default=os.path.join(os.getcwd(), 'wine.xlsx'),
+        help='директория с файлом wine.xlsx (по умолчанию - ПУТЬ_К_ПАПКЕ_СО_СКРИПТОМ/wine.xlsx)'
+    )
+
+    path = wine_parser.parse_args().path
+    try:
+        wines = get_wines_from_file(path)
+    except (FileNotFoundError, ValueError) as error:
+        print(f'Неверно указан путь к файлу.\nОшибка: {error}')
+        environs = Env()
+        try:
+            environs.read_env("setup.txt", recurse=False)
+            path = environs.str('PATH_TO_WINE_FILE')
+            wines = get_wines_from_file(path)
+        except (FileNotFoundError, ValueError) as error:
+            print(f'setup.txt в корневой папке не найден или в нем не указан путь к файлу вин в PATH_TO_WINE_FILE.\n'
+                  f'Ошибка: {error}')
+            exit()
+
+    print(f'Сайт запущен с файлом базы данных {path}')
     new_wine_set = collections.defaultdict(list)
     for wine in wines:
         new_wine_set[wine['Категория']].append(wine)
